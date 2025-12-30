f1bb0c45a5cc8a09a6afcf8f0b5d87c1a709f51d
JesseLine <linej8765@gmail.com>
2025-12-30 13:15:54 -0500

    add EventBus script and gravity

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,15 @@ public class PlayerMovement : MonoBehaviour
 
     public float walkingSpeed = 10f;
     public float runningSpeed = 15f;
+    public float gravity = -10f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
 
     private float speed;
+    private Vector3 velocity;
+    bool isGrounded;
 
     private CharacterController controller;
     private Vector3 moveVelocity;
@@ -20,6 +27,13 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
@@ -34,5 +48,12 @@ public class PlayerMovement : MonoBehaviour
             speed = walkingSpeed;
         }
         controller.Move(move * speed * Time.deltaTime);
+
+        //gravity
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        
     }
 }
