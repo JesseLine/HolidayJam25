6880604e4177eb52330a1883916dfc8eb46cc7c2
JesseLine <linej8765@gmail.com>
2025-12-30 21:07:24 -0500

    starting to build first level

--- Assets/ElfMovement.cs
+++ Assets/ElfMovement.cs
@@ -3,7 +3,16 @@ using UnityEngine;
 public class ElfMovement : MonoBehaviour
 {
     public float speed = 10f;
+    public float chaseSpeed = 15f;
+    public float rotationSpeed = 5f;
     public Transform[] waypoints;
+
+    public float viewRange = 5f;
+    public float viewAngle = 45f;
+    public Transform player;
+    public Transform viewingCone;
+
+    bool playerInView;
 
     int currentWaypointIndex = 0;
     Rigidbody rb;
@@ -13,12 +22,23 @@ public class ElfMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        playerInView = false;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Patrol();
+        PlayerInView();
+        if (playerInView)
+        {
+            Chase();
+        }
+        else
+        {
+            Patrol();
+        }
+        
+
     }
 
     void Patrol()
@@ -35,12 +55,51 @@ public class ElfMovement : MonoBehaviour
 
         Vector3 direction = (targetPos - currentPos).normalized;
 
+        direction.y = 0f;
+
+        if (direction.sqrMagnitude > 0.001f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+
         rb.MovePosition(transform.position + direction * speed * Time.deltaTime);
 
         if(Vector3.Distance(currentPos, targetPos) < 0.1f)
         {
             currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
         }
+    }
+
+    void Chase()
+    {
+        Debug.Log("Chasing!");
+        Vector3 currentPos = transform.position;
+        Vector3 targetPos = player.position;
+
+        Vector3 direction = (targetPos - currentPos).normalized;
+
+        if(direction.sqrMagnitude > 0.001f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+
+        rb.MovePosition(transform.position + direction * chaseSpeed * Time.deltaTime);
+    }
+
+    void PlayerInView()
+    {
+        if (viewingCone.GetComponent<MeshCollider>().bounds.Intersects(player.GetComponent<CapsuleCollider>().bounds))
+        {
+            Debug.Log("Player in view");
+            playerInView = true;
+        }
+        else
+        {
+            playerInView = false;
+        }
+
     }
 
     private void OnDrawGizmos()
